135355287f888531bda05197fa910c61bcee0433
neulandp <neulandp074@gmail.com>
2024-11-15 19:21:58 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let acessarClick = document.getElementById("botaoClick"); //Seleciona o elemento do HTML e armazena em uma variável
+
+acessarClick.addEventListener('click', (e) => { //Adiciona um evento para o botão selecionado
+    e.preventDefault(); //Apenas para manter no console e a mensagem não sumir
+    console.log("O click deu certo"); //Exibe no console toda vez que o botão for clicado
+})
